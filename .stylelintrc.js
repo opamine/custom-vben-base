3b238839422326d27b8e4316fd3219bf03c424cc
清禾 <zzhou998@163.com>
2022-05-02 16:24:43 +0800

    perf: stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,10 +3,12 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   plugins: ['stylelint-order'],
   rules: {
-    'font-family-no-missing-generic-family-keyword': null,
+    'declaration-colon-newline-after': null, // 与 prettier 冲突，以 prettier 为准
+    'value-list-comma-newline-after': null, // 与 prettier 冲突，以 prettier 为准
+    'font-family-no-missing-generic-family-keyword': null, // 与 prettier 冲突，以 prettier 为准
     'no-descending-specificity': null,
     'function-no-unknown': null,
-    'selector-class-pattern': null, // 即使关闭类名校验，请优先使用 'aaa-bbb' 格式
+    'selector-class-pattern': null, // 即使关闭类名校验，请优先使用 'lowercase-lowercase' 格式
     'string-quotes': 'single', // stylelint-config-standard 中配置的是 'double', prettier 中配置 'single', 手动以 prettier 为准
     'order/order': [
       [
