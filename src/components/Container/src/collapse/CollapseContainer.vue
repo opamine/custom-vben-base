ffa4921e26de064e7c63e1de866b0f07741e231a
清禾 <zzhou998@163.com>
2023-03-29 22:29:45 +0800

    fix:  CollapseHeader.vue

--- src/components/Container/src/collapse/CollapseContainer.vue
+++ src/components/Container/src/collapse/CollapseContainer.vue
@@ -1,118 +1,44 @@
 <script lang="tsx">
-  import { ref, unref, defineComponent, type PropType, type ExtractPropTypes } from 'vue';
-  import { isNil } from 'lodash-es';
-  import { Skeleton } from 'ant-design-vue';
-  import { CollapseTransition } from '/@/components/Transition';
-  import CollapseHeader from './CollapseHeader.vue';
-  import { triggerWindowResize } from '/@/utils/event';
-  import { useTimeoutFn } from '/@/hooks/core/useTimeout';
+  import { defineComponent, computed, unref, type ExtractPropTypes } from 'vue';
   import { useDesign } from '/@/hooks/web/useDesign';
+  import { BasicArrow, BasicTitle } from '/@/components/Basic';
 
-  const collapseContainerProps = {
-    title: { type: String, default: '' },
-    loading: { type: Boolean },
-    /**
-     *  Can it be expanded
-     */
-    canExpan: { type: Boolean, default: true },
-    /**
-     * Warm reminder on the right side of the title
-     */
+  const collapseHeaderProps = {
+    prefixCls: String,
+    title: String,
+    show: Boolean,
+    canExpan: Boolean,
     helpMessage: {
       type: [Array, String] as PropType<string[] | string>,
       default: '',
     },
-    /**
-     * Whether to trigger window.resize when expanding and contracting,
-     * Can adapt to tables and forms, when the form shrinks, the form triggers resize to adapt to the height
-     */
-    triggerWindowResize: { type: Boolean },
-    /**
-     * Delayed loading time
-     */
-    lazyTime: { type: Number, default: 0 },
   };
 
-  export type CollapseContainerProps = ExtractPropTypes<typeof collapseContainerProps>;
+  export type CollapseHeaderProps = ExtractPropTypes<typeof collapseHeaderProps>;
 
   export default defineComponent({
-    name: 'CollapseContainer',
-
-    props: collapseContainerProps,
-
-    setup(props, { expose, slots }) {
+    name: 'CollapseHeader',
+    inheritAttrs: false,
+    props: collapseHeaderProps,
+    emits: ['expand'],
+    setup(props, { slots, attrs, emit }) {
       const { prefixCls } = useDesign('collapse-container');
-
-      const show = ref(true);
-
-      const handleExpand = (val: boolean) => {
-        show.value = isNil(val) ? !show.value : val;
-        if (props.triggerWindowResize) {
-          // 200 milliseconds here is because the expansion has animation,
-          useTimeoutFn(triggerWindowResize, 200);
-        }
-      };
-
-      expose({ handleExpand });
-
+      const _prefixCls = computed(() => props.prefixCls || unref(prefixCls));
       return () => (
-        <div class={unref(prefixCls)}>
-          <CollapseHeader
-            {...props}
-            prefixCls={unref(prefixCls)}
-            onExpand={handleExpand}
-            show={show.value}
-            v-slots={{
-              title: slots.title,
-              action: slots.action,
-            }}
-          />
+        <div class={[`${unref(_prefixCls)}__header px-2 py-5`, attrs.class]}>
+          <BasicTitle helpMessage={props.helpMessage} normal>
+            {slots.title?.() || props.title}
+          </BasicTitle>
 
-          <div class='p-2'>
-            <CollapseTransition enable={props.canExpan}>
-              {props.loading ? (
-                <Skeleton active={props.loading} />
-              ) : (
-                <div class={`${prefixCls}__body`} v-show={show.value}>
-                  {slots.default?.()}
-                </div>
-              )}
-            </CollapseTransition>
+          <div class={`${unref(_prefixCls)}__action`}>
+            {slots.action
+              ? slots.action({ expand: props.show, onClick: () => emit('expand') })
+              : props.canExpan && (
+                  <BasicArrow up expand={props.show} onClick={() => emit('expand')} />
+                )}
           </div>
-
-          {slots.footer && <div class={`${prefixCls}__footer`}>{slots.footer()}</div>}
         </div>
       );
     },
   });
 </script>
-
-<style lang="less">
-  @prefix-cls: ~'@{namespace}-collapse-container';
-
-  .@{prefix-cls} {
-    background-color: @component-background;
-    border-radius: 2px;
-    transition: all 0.3s ease-in-out;
-
-    &__header {
-      display: flex;
-      height: 32px;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid @border-color-light;
-    }
-
-    &__footer {
-      border-top: 1px solid @border-color-light;
-    }
-
-    &__action {
-      display: flex;
-      text-align: right;
-      flex: 1;
-      align-items: center;
-      justify-content: flex-end;
-    }
-  }
-</style>
