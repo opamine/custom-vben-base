ef2a9f0f121c993b7a907d4ed1d3daa59e3e089e
清禾 <zzhou998@163.com>
2023-04-02 21:07:32 +0800

    chore: css is no longer imported on demand

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'virtual:windi-base.css';
 import 'virtual:windi-components.css';
 import '/@/design/index.less';
 import 'virtual:windi-utilities.css';
+import 'ant-design-vue/dist/antd.css';
 // Register icon sprite
 import 'virtual:svg-icons-register';
 import App from './App.vue';
@@ -14,12 +15,6 @@ import { setupStore } from '/@/store';
 import { setupGlobDirectives } from '/@/directives';
 import { setupI18n } from '/@/locales/setupI18n';
 import { registerGlobComp } from '/@/components/registerGlobComp';
-
-import { isDevMode } from './utils/env';
-
-if (isDevMode()) {
-  import('ant-design-vue/es/style');
-}
 
 async function bootstrap() {
   const app = createApp(App);
